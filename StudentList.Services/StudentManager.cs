d564d9e556e78128599817e29198a71f5ad14c77
Andrew Byrne <anbyrne@microsoft.com>
2017-06-14 22:00:21 -0700

    finish tests

--- StudentList.Services/StudentManager.cs
+++ StudentList.Services/StudentManager.cs
@@ -1,14 +1,16 @@
 using System;
-
+using System.Linq;
 
 namespace StudentList.Services
 {
     public class StudentManager
     {
         private StudentStorage _storage;
+        private Random _rand;
         public StudentManager(StudentStorage storage)
         {
             _storage = storage;
+            _rand = new Random();
         }
 
         public string[] GetAllStudents()
@@ -21,6 +23,35 @@ namespace StudentList.Services
         {
            var studentList = _storage.LoadStudentList();
            return studentList.Split(',').Length;
+        }
+
+        public string PickRandomStudent()
+        {
+            var studentList = _storage.LoadStudentList();
+            var students = studentList.Split(',');
+            var randomIndex = _rand.Next(0,students.Length);
+            return students[randomIndex];
+        }
+
+        public void AddStudent(string newStudent)
+        {
+            var studentList = _storage.LoadStudentList();
+            _storage.UpdateStudentList(studentList + "," + newStudent);
+        }
+
+        public bool StudentExists(string student)
+        {
+            var studentList = _storage.LoadStudentList();
+            var students = studentList.Split(',');
+
+            // Using the 'Any'  LINQ method to return whether or not 
+            // any item  matches the given predicate.
+            if (students.Any(s => s.Trim() == student))
+            {
+                return true;
+            }
+
+            return false;
         }
 
     }
--- StudentList.Tests/StudentManager_Should.cs
+++ StudentList.Tests/StudentManager_Should.cs
@@ -45,5 +45,63 @@ namespace StudentList.Tests
             Assert.Equal(actual, 3);
 
         }
+
+        [Fact]
+        public void ReturnRandomStudent()
+        {
+            // Arrange 
+            var sut = new StudentManager(mockStorage.Object);
+            var actualString = mockStorage.Object.LoadStudentList();
+
+            // Act
+            var expectedSubString = sut.PickRandomStudent();
+
+            // Assert
+            Assert.Contains(expectedSubString,actualString);
+        }
+
+        [Fact]
+        public void Call_UpdateStudentList_When_StudentAdded()
+        {
+            // Arrange 
+            var sut = new StudentManager(mockStorage.Object);
+            var originalList = mockStorage.Object.LoadStudentList();
+            var newStudent = "testStudent";
+            var updatedList = originalList + "," + newStudent;
+            
+            // Act
+            sut.AddStudent(newStudent);
+            
+            // Assert
+            mockStorage.Verify(x => x.UpdateStudentList(updatedList));
+        }
+
+        [Fact]
+        public void ReturnTrue_When_SearchForExistingStudent()
+        {
+            // Arrange 
+            var sut = new StudentManager(mockStorage.Object);
+            var existingStudent = "student1"; // This student is setup in mock StudentStorage object.
+
+            // Act
+            var actual = sut.StudentExists(existingStudent);
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void ReturnFalse_When_SearchForNonExistentStudent()
+        {
+            // Arrange 
+            var sut = new StudentManager(mockStorage.Object);
+            var fakeStudent = "notARealStudent"; 
+
+            // Act
+            var actual = sut.StudentExists(fakeStudent);
+
+            // Assert
+            Assert.False(actual);
+        }
     }
 }
